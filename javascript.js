984bd215053977d81a8c329c3e0342d38ce0f97b
Vullum <markus.yung.vullum@gmail.com>
2022-09-26 17:36:55 +0200

    v

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const hamMen = document.querySelector('.fa-solid.fa-bars')
 const navLink = document.querySelector('.navbar-links')
 
-hamMen.addEventListener("onclick", ()=>{
+hamMen.addEventListener('click',()=>{
   navLink.classList.toggle('fa-solid.fa-bars')
 })

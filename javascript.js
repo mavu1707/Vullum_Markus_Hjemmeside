78d0f470c515642858265419b741507f2f4b0390
Vullum <markus.yung.vullum@gmail.com>
2022-09-29 10:34:05 +0200

    Animasjoner

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
-const hamMen = document.querySelector('.fa-solid.fa-bars')
-const navLink = document.querySelector('.navbar-links')
+/*Hentet fra: https://www.youtube.com/channel/UC2Xd-TjJByJyK2w1zNwY0zQ
+*/
 
-hamMen.addEventListener('click',()=>{
-  navLink.classList.toggle('fa-solid.fa-bars')
-})
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry)=>{
+    console.log(entry);
+    if (entry.isIntersecting){
+      entry.target.classList.add('show')
+    }else{
+      entry.target.classList.remove('show')
+    }
+  });
+});
+
+const artikler = document.querySelectorAll('.artikler');
+
+artikler.forEach((el) => observer.observe(el))

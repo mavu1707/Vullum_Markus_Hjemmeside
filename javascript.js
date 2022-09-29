0114b09817fde41dff4f138ec0209906d55f8ece
Vullum <markus.yung.vullum@gmail.com>
2022-09-29 12:58:37 +0200

    Hamburger meny

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 /*Hentet fra: https://www.youtube.com/channel/UC2Xd-TjJByJyK2w1zNwY0zQ
 */
-
 const observer = new IntersectionObserver((entries) =>{
   entries.forEach((entry)=>{
     console.log(entry);
@@ -15,3 +14,20 @@ const observer = new IntersectionObserver((entries) =>{
 const artikler = document.querySelectorAll('.artikler');
 
 artikler.forEach((el) => observer.observe(el))
+
+
+/* Hentet fra: https://www.youtube.com/watch?v=flItyHiDm7E&t=205s*/
+const hamburger = document.querySelector('.hamburger')
+const navbarLinks = document.querySelector('.navbar-links')
+
+/*Dette er for hva som skal skje med klassen bar og selve navbar*/
+hamburger.addEventListener("click", () =>{
+  hamburger.classList.toggle("active")
+  navbarLinks.classList.toggle("active")
+})
+
+/*Dette skjuler selve navbar dersom jeg trykker på en av linkene mens jeg er i navbar*/
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+  hamburger.classList.remove("active")
+  navbarLinks.classList.remove("active")
+}))
